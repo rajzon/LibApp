8d77f2f19aa8f05fc4a5903e3419586e7fc657c4
Rajzon <dawid.nosek@microsoft.wsei.edu.pl>
2021-03-17 15:00:04 +0100

    Prepared IndentityServer startup config with EF and SQL AB#235

--- Identity.API/Startup.cs
+++ Identity.API/Startup.cs
@@ -1,6 +1,10 @@
+using Identity.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,10 +12,48 @@ namespace Identity.API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = _config.GetConnectionString("DefaultConnection");
+            var migrationAssembly = typeof(Startup).Assembly.GetName().Name;
+            services.AddDbContext<ApplicationDbContext>(config =>
+            {
+                config.UseSqlServer(connectionString);
+            });
+            
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            
+            
+            services.AddIdentityServer()
+                .AddAspNetIdentity<IdentityUser>()
+                // this adds the config data from DB (clients, resources)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString, 
+                            sql => sql.MigrationsAssembly(migrationAssembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString, 
+                            sql => sql.MigrationsAssembly(migrationAssembly));
+                })
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,6 +64,8 @@ namespace Identity.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseIdentityServer();
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
