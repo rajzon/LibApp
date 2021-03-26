4f9af58a2a712cdb63acf0b45240713e271277da
Rajzon <dawid.nosek@microsoft.wsei.edu.pl>
2021-03-26 16:28:51 +0100

    updated links on sidebar

--- Web/OrganisationSPA/src/app/core/core.module.ts
+++ Web/OrganisationSPA/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavComponent} from "./components/nav/nav.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [NavComponent],
@@ -8,7 +9,8 @@ import {NavComponent} from "./components/nav/nav.component";
     NavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class CoreModule { }
