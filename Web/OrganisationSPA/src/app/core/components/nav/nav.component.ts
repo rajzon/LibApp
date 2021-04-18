db3410e804946a3e1fdf1fbbac193b732ed86da0
Rajzon <dawid.nosek@microsoft.wsei.edu.pl>
2021-04-18 17:05:58 +0200

    showing userName in navbar AB#277,

--- Web/OrganisationSPA/src/app/core/components/nav/nav.component.ts
+++ Web/OrganisationSPA/src/app/core/components/nav/nav.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 // @ts-ignore
-import { environment } from '@env';
+import {environment} from '@env';
 import {AuthService} from "@core/services/auth.service";
+import {map} from "rxjs/operators";
+import {UserInfo} from "angular-oauth2-oidc";
+import {Observable} from "rxjs";
 
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.sass']
+  styleUrls: ['./nav.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavComponent implements OnInit {
 
-  userNamePlaceholder: string = "UserName";
+  userName: string
   logoImgSrc: string = environment.logoImg;
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    console.log('NAV INIT');
+  }
+
+  getUserName$(): Observable<string> {
+    return this.authService.getUserInfo$().pipe(map((res: UserInfo) => {
+      console.log(res);
+      return res?.name
+    }));
   }
 
   logout(): void {
