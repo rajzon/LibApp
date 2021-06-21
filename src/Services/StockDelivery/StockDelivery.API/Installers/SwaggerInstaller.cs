9ced04ec5b39cc65ba220a7bf0fc19f6be12e2be
Rajzon <dawid.nosek@microsoft.wsei.edu.pl>
2021-06-21 14:54:27 +0200

    added Docker image and swagger support for DeliveryStock service AB#346 AB#348

---
+++ src/Services/StockDelivery/StockDelivery.API/Installers/SwaggerInstaller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace StockDelivery.API.Installers
+{
+    public static class SwaggerInstaller
+    {
+        public static IServiceCollection AddSwaggerInitializer(this IServiceCollection services)
+        {
+            return services.AddSwaggerGen(c =>
+            {
+                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerDoc(description.ApiVersion.ToString(), new OpenApiInfo()
+                    {
+                        Title = "StockDelivery API",
+                        Version = description.ApiVersion.ToString()
+                    });
+
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Description =
+                            "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
+                    });
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
